dc17565f3d6a75db70d4d85d5607e924ed87596b
Hunter <hunter.monk@gmail.com>
2016-01-08 23:25:04 -0600

    Connects to self!

--- ThreeWayFaceTime/MainViewController.swift
+++ ThreeWayFaceTime/MainViewController.swift
@@ -13,9 +13,9 @@ class MainViewController: UIViewController {
     // Replace with your OpenTok API key
     let apiKey = "45454712"
     // Replace with your generated session ID
-    let sessionId = "2_MX40NTQ1NDcxMn5-MTQ1MTc5NDk2NjU1M35mcGkvWnpiVUJtZU1ySHVxU0pONjMxR1N-UH4"
+    let sessionId = "2_MX40NTQ1NDcxMn5-MTQ1MjMxNjg3NjMyMH5XbDNObHNoRDRzUk9xbHRSRWNTaGRwaCt-UH4"
     // Replace with your generated token
-    let token = "T1==cGFydG5lcl9pZD00NTQ1NDcxMiZzaWc9MjIyZmVmZTRjZThiNzA2MDhiOTJlYTc3MjJhYTY2YTVlM2VlZWExNTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5UUTFORGN4TW41LU1UUTFNVGM1TkRrMk5qVTFNMzVtY0drdlducGlWVUp0WlUxeVNIVnhVMHBPTmpNeFIxTi1VSDQmY3JlYXRlX3RpbWU9MTQ1MTc5NDk3MyZub25jZT0wLjI2MTA0OTQ3OTk0Nzc3MTA2JmV4cGlyZV90aW1lPTE0NTE4ODEzNTUmY29ubmVjdGlvbl9kYXRhPQ=="
+    let token = "T1==cGFydG5lcl9pZD00NTQ1NDcxMiZzaWc9ODUzZjVkMzFlYjQ4M2JlMjZkYTQxMTI4NWJjMTMxMzhkMmQ1ODI2Mjpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5UUTFORGN4TW41LU1UUTFNak14TmpnM05qTXlNSDVYYkROT2JITm9SRFJ6VWs5eGJIUlNSV05UYUdSd2FDdC1VSDQmY3JlYXRlX3RpbWU9MTQ1MjMxNjg5NCZub25jZT0wLjgyMDcxOTQ2NDQ5MTQ0OTUmZXhwaXJlX3RpbWU9MTQ1NDkwODg2MSZjb25uZWN0aW9uX2RhdGE9"
     
     let screenSize = UIScreen.mainScreen().bounds
     
@@ -33,7 +33,6 @@ class MainViewController: UIViewController {
     func connectSession() {
         print("connectSession")
         var error:OTError? = nil
-        
         
         session.connectWithToken(token, error: &error)
         
@@ -112,15 +111,6 @@ extension MainViewController: OTSubscriberKitDelegate {
         
         globalSubscriber.view.frame = CGRectMake(0, 200, screenSize.width, 200)
         view.addSubview(globalSubscriber.view)
-        /*
-        let actualSubscriber = OTSubscriber(stream: subscriber.stream, delegate: self)
-        
-        
-        actualSubscriber.viewScaleBehavior = .Fit
-        
-        actualSubscriber.view.frame = CGRectMake(0, 200, screenSize.width, 200)
-        
-        view.addSubview(actualSubscriber.view)*/
     }
     
     func subscriber(subscriber: OTSubscriberKit!, didFailWithError error: OTError!) {
