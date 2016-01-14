26b23d14fd5904a4f82d0a9e1f540a99aed55204
Hunter <hunter.monk@gmail.com>
2016-01-13 23:11:18 -0600

    Initial commit

---
+++ ThreeWayFaceTime/ContactTableViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ContactTableViewController.swift
+//  ThreeWayFaceTime
+//
+//  Created by Hunter Maximillion Monk on 1/13/16.
+//  Copyright © 2016 Hunter Maximillion Monk. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewController: UITableViewController {
+    
+    var contacts:[FTContact] = []
+    
+    class func instantiateFromStoryboard() -> ContactTableViewController {
+        return UIStoryboard(name: "Contact", bundle: nil).instantiateInitialViewController() as! ContactTableViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contacts = CoreData.sharedInstance.fetchAllContacts()
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return contacts.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        
+        if contacts.count != 0 {
+            let contact = contacts[indexPath.row]
+            cell.textLabel?.text = "\(contact.givenName) \(contact.familyName)"
+        }
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
