35a64a6c093b6e804b63ee59cb34dd6793217808
Hunter <hunter.monk@gmail.com>
2016-01-13 23:10:32 -0600

    Add class

---
+++ ThreeWayFaceTime/FTContact.swift
@@ -0,0 +1,26 @@
+//
+//  FTContact.swift
+//  ThreeWayFaceTime
+//
+//  Created by Hunter Maximillion Monk on 1/13/16.
+//  Copyright © 2016 Hunter Maximillion Monk. All rights reserved.
+//
+
+import Foundation
+
+class FTContact: NSObject {
+    
+    let givenName:String
+    let familyName:String
+    let phoneNumber:String
+    
+    init(givenName:String,familyName:String,phoneNumber:String) {
+        self.givenName = givenName
+        self.familyName = familyName
+        self.phoneNumber = phoneNumber
+        super.init()
+    }
+    
+    
+    
+}
