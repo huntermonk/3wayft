8c7454967cbf6e2ea820a9b248f9e21d1e115d1e
Hunter <hunter.monk@gmail.com>
2016-01-24 20:29:47 -0600

    Initial commit

---
+++ ThreeWayFaceTime/RecentsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecentsTableViewCell.swift
+//  ThreeWayFaceTime
+//
+//  Created by Hunter Maximillion Monk on 1/24/16.
+//  Copyright © 2016 Hunter Maximillion Monk. All rights reserved.
+//
+
+import UIKit
+
+class RecentsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
