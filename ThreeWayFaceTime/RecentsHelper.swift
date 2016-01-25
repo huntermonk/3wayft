ce368e60877585885deccd8ac072e9c38ea29c12
Hunter <hunter.monk@gmail.com>
2016-01-24 20:30:07 -0600

    Initial commit

--- ThreeWayFaceTime/RecentsHelper.swift
+++ ThreeWayFaceTime/RecentsHelper.swift
@@ -7,11 +7,39 @@
 //
 
 import Foundation
+import Parse
+
+protocol RecentsHelperDelegate {
+    func displayRecents(calls:[PFObject])
+}
 
 class RecentsHelper: NSObject {
     
+    var delegate:RecentsHelperDelegate?
+    
     static let sharedInstance = RecentsHelper()
     
+    var recents = [PFObject]()
     
+    override init() {
+        super.init()
+        retrieveRecents()
+    }
     
+    func retrieveRecents() {
+        let initiated = PFQuery(className:"Call")
+        initiated.whereKey("initiator", equalTo: PFUser.currentUser()!.username!)
+        
+        let received = PFQuery(className: "Call")
+        received.whereKey("receiver", equalTo: PFUser.currentUser()!.username!)
+        
+        let combined = PFQuery.orQueryWithSubqueries([initiated, received])
+        combined.findObjectsInBackgroundWithBlock { (results, error) -> Void in
+            UIAlertController().displayError(error)
+            if results != nil {
+                self.recents = results!
+                self.delegate?.displayRecents(self.recents)
+            }
+        }
+    }
 }
