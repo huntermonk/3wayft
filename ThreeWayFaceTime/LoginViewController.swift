3e016fcd5a59c4e0be996bb7119e0f320ef681a7
Hunter <hunter.monk@gmail.com>
2016-01-25 19:16:33 -0600

    Initial commit

--- ThreeWayFaceTime/LoginViewController.swift
+++ ThreeWayFaceTime/LoginViewController.swift
@@ -1,86 +1,136 @@
 //
 //  LoginViewController.swift
 //  ThreeWayFaceTime
-//
-//  Created by Hunter Maximillion Monk on 1/11/16.
-//  Copyright © 2016 Hunter Maximillion Monk. All rights reserved.
-//
 
 import UIKit
-import DigitsKit
+import Bolts
 import Parse
-import Contacts
 
 class LoginViewController: UIViewController {
-    
-    class func instantiateFromStoryboard() -> LoginViewController {
-        return UIStoryboard(name: "Login", bundle: nil).instantiateInitialViewController() as! LoginViewController
-    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addDigitsButton()
-    }
-    
-    func addDigitsButton() {
-        
-        let digitsButton = DGTAuthenticateButton(authenticationCompletion: {
-            (session, error) in
-            if error != nil {
-                UIAlertController().displayError(error)
-            } else {
-                self.loginParse()
-            }
-            
-        })
-                
-        digitsButton.center = view.center
-        self.view.addSubview(digitsButton)
-        
-    }
-    
-    func loginParse() {
-        
-        PFUser.loginWithDigitsInBackground({ (user, error) -> Void in
-            if error != nil {
-                UIAlertController().displayError(error)
-            } else {
-                self.uploadContacts()
-            }
-        })
-        
-    }
-    
-    func dismiss() {
-        
-        if let controller = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() {
-            presentViewController(controller, animated: true) { () -> Void in
-                self.dismissViewControllerAnimated(true, completion: nil)
-            }
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var sendCodeButton: UIButton!
+
+  @IBOutlet weak var questionLabel: UILabel!
+  @IBOutlet weak var subtitleLabel: UILabel!
+
+  var phoneNumber: String = ""
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    step1()
+    sendCodeButton.layer.cornerRadius = 3
+
+    self.editing = true
+  }
+
+  func step1() {
+    phoneNumber = ""
+    textField.placeholder = NSLocalizedString("numberDefault", comment: "555-333-6726")
+    questionLabel.text = NSLocalizedString("enterPhone", comment: "Please enter your phone number to log in.")
+    subtitleLabel.text = NSLocalizedString("enterPhoneExtra", comment: "This example is limited to 10-digit US number.")
+    sendCodeButton.enabled = true
+  }
+
+  func step2() {
+    phoneNumber = textField.text!
+    textField.text = ""
+    textField.placeholder = "1234"
+    questionLabel.text = NSLocalizedString("enterCode", comment: "Enter the 4-digit confirmation code:")
+    subtitleLabel.text = NSLocalizedString("enterCodeExtra", comment: "It was sent in an SMS message to +1 " + phoneNumber) + phoneNumber
+    sendCodeButton.enabled = true
+  }
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+
+    textField.becomeFirstResponder()
+  }
+
+  @IBAction func didTapSendCodeButton() {
+    let preferredLanguage = NSBundle.mainBundle().preferredLocalizations[0]
+
+    let textFieldText = textField.text ?? ""
+
+    if phoneNumber == "" {
+      if (preferredLanguage == "en" && textFieldText.characters.count != 10)
+        || (preferredLanguage == "ja" && textFieldText.characters.count != 11) {
+          showAlert("Phone Login", message: NSLocalizedString("warningPhone", comment: "You must enter a 10-digit US phone number including area code."))
+          return step1()
+      }
+
+      self.editing = false
+      let params = ["phoneNumber" : textFieldText, "language" : preferredLanguage]
+      PFCloud.callFunctionInBackground("sendCode", withParameters: params) { response, error in
+        self.editing = true
+        if let error = error {
+          var description = error.description
+          if description.characters.count == 0 {
+            description = NSLocalizedString("warningGeneral", comment: "Something went wrong. Please try again.") // "There was a problem with the service.\nTry again later."
+          } else if let message = error.userInfo["error"] as? String {
+            description = message
+          }
+          self.showAlert("Login Error", message: description)
+          return self.step1()
         }
-        
-    }
-    
-    // TODO: - Get all this logic out to the model
+        return self.step2()
+      }
+    } else {
+      if textFieldText.characters.count == 4, let code = Int(textFieldText) {
+        return doLogin(phoneNumber, code: code)
+      }
 
-    func uploadContacts() {
-        let userSession = Digits.sharedInstance().session()
-        let contacts = DGTContacts(userSession: userSession)
-        
-        contacts.startContactsUploadWithCompletion {
-            result, error in
-            if error != nil {
-                print("error \(error.debugDescription)")
-                if error.code != 8 {
-                    UIAlertController().displayError(error)
-                } else {
-                    self.dismiss()
-                }
-            } else {
-                self.dismiss()
-            }
+      showAlert("Code Entry", message: NSLocalizedString("warningCodeLength", comment: "You must enter the 4 digit code texted to your phone number."))
+    }
+  }
+
+  func doLogin(phoneNumber: String, code: Int) {
+    self.editing = false
+    let params = ["phoneNumber": phoneNumber, "codeEntry": code] as [NSObject:AnyObject]
+    PFCloud.callFunctionInBackground("logIn", withParameters: params) { response, error in
+      if let description = error?.description {
+        self.editing = true
+        return self.showAlert("Login Error", message: description)
+      }
+      if let token = response as? String {
+        print("token\(token)")
+        PFUser.becomeInBackground(token) { user, error in
+          if let _ = error {
+            self.showAlert("Login Error", message: NSLocalizedString("warningGeneral", comment: "Something happened while trying to log in.\nPlease try again."))
+            self.editing = true
+            return self.step1()
+          }
+          return self.dismissViewControllerAnimated(true, completion: nil)
         }
+      } else {
+        self.editing = true
+        self.showAlert("Login Error", message: NSLocalizedString("warningGeneral", comment: "Something went wrong.  Please try again."))
+        return self.step1()
+      }
     }
-    
+  }
 
+  override func setEditing(editing: Bool, animated: Bool) {
+    sendCodeButton.enabled = editing
+    textField.enabled = editing
+    if editing {
+      textField.becomeFirstResponder()
+    }
+  }
+
+  func showAlert(title: String, message: String) {
+    return UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: NSLocalizedString("alertOK", comment: "OK")).show()
+  }
+
+  override func preferredStatusBarStyle() -> UIStatusBarStyle {
+    return .Default
+  }
+}
+
+extension LoginViewController : UITextFieldDelegate {
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    self.didTapSendCodeButton()
+    
+    return true
+  }
 }
